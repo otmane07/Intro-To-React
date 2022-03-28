6e0c902b3b64563024205b5abbe0b40166d8da78
Khouna Otmane <otmanekhouna07@gmail.com>
2022-03-28 19:48:00 +0200

    update

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import SearchParams from "./SearchParams";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import Pet from "./Pet";
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function App() {
+  console.log("hello");
+  return (
+    <div>
+      <h1 className="adopt-me">Adopt me</h1>
+      <SearchParams />;
+      {/* <Pet name="Luna" animal="dog" breed="Havanese" />
+      <Pet name="Pepper" animal="bird" breed="Cockatiel" />
+      <Pet name="Doink" animal="cat" breed="Mix" /> */}
+    </div>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
